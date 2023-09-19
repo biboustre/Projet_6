1cd246fcddfe778336791b17448ab5ea522cff84
MAAMAR BENHADJAR <biboustre@hotmail.com>
2023-09-19 16:03:29 +0200

    categorie creation

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -9,10 +9,10 @@
        let description = document.createElement('figcaption');
        description.innerText = projet.title;
        fig.appendChild(img);
-       fig.appendChild(description); 
-       document.getElementById('gallery').appendChild(fig);
-       
+       fig.appendChild(description);
+       document.querySelector('.gallery').appendChild(fig);
     }));
+
 
 
 
@@ -22,19 +22,69 @@ travauxFiltrer.classList.add('filter_travaux')
 document.querySelector('#portfolio').appendChild(travauxFiltrer);
 
 
-
 for (let i = 0; i < 4; i++) {
-    const categorie = document.createElement('div');
+    const categorie = document.createElement('button');
     categorie.className = 'select';
     categorie.id = 'select' +i;
     document.querySelector('.filter_travaux').appendChild(categorie);
 }
 
+document.querySelector('#select0').innerText = 'Tous';
+document.querySelector('#select1').innerText = 'Objets';
+document.querySelector('#select2').innerText = 'Appartements';
+document.querySelector('#select3').innerText = 'Hôtels & restaurants';
 
-let texte = document.createElement('p');
-texte.classList.add('texte');                                                   
-texte.id = 'texte';
-document.querySelectorAll('.select').appendChild(texte);      
+
+let gallery = document.createElement('div');
+gallery.classList.add('gallery');
+document.getElementById('portfolio').appendChild(gallery);
+
+
+
+
+//filtrer les travaux
+// const arrayResponse = await fetch('')
+// const filterTous = document.querySelector('#select0');
+
+// filterTous.addEventListener('click', function(){
+//     const tous = 
+// })
+
+
+// fetch('http://localhost:5678/api/categories')
+//     .then(res => res.json())
+//     .then(data => data.forEach(category => {
+//         console.log(category);
+//         const tous = document.querySelector('#select0');
+        
+//     tous.addEventListener('click', function(){
+//         const travauxFilter = category.filter(function(categories){
+//         return categories.category >= 0;
+//         console.log(travauxFilter);
+//         })
+//     })
+// }))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -45,25 +95,20 @@ document.querySelectorAll('.select').appendChild(texte);
 //     document.querySelector('.filter_travaux').appendChild(categorie);
 // }
 
+
+// let texte = document.createElement('p');
+// texte.classList.add('texte');     
+// document.querySelector('.select').appendChild(texte);      
+
+
 //     let texte = document.createElement('p');
 //     texte.classList.add('texte');
 // document.querySelectorAll('.select').appendChild(texte);
 
 
 
-// document.querySelector('texte').innerText = 'Tous';
-// document.querySelector('texte').innerText = 'Objets';
-// document.querySelector('texte').innerText = 'Appartements';
-// document.querySelector('texte').innerText = 'Hôtels & restaurants';
 
 
-
-
-
-
-
-
-    
 // fetch('http://localhost:5678/api/works')
 //     .then((res => res.json()))
 //     .then((data => data.filter()))
