213679e9fcd552104cbd13300a4c39ae51d687cc
MAAMAR BENHADJAR <biboustre@hotmail.com>
2023-09-19 23:40:55 +0200

    page connexion

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -1,7 +1,18 @@
- fetch('http://localhost:5678/api/works')
-    .then(res => res.json())                        //transformer au format json
-    .then(data => data.forEach(projet => {          //Récuperation des données et boucle pour inserer les travaux dans le HTML
-        console.log(projet);
+ let projects = [];
+
+fetch('http://localhost:5678/api/works')
+    .then(res => res.json())
+    .then((data) => {
+        projects = data;
+        console.log("je suis 1er");
+        displayGallery(projects);
+    }) 
+
+
+    
+function displayGallery(datas){
+    document.querySelector('.gallery').innerHTML = "";
+    datas.forEach(projet => { 
        let fig = document.createElement('figure');
        let img = document.createElement('img');
        img.src = projet.imageUrl;
@@ -11,8 +22,20 @@
        fig.appendChild(img);
        fig.appendChild(description);
        document.querySelector('.gallery').appendChild(fig);
-    }));
+    })
+}
 
+
+console.log("je suis 2ème");
+console.log(projects);
+
+const cat1 = projects.filter(function(project) {
+   return project;
+});
+
+function filtreCategory(id, datas){
+    return datas.filter((data) => data.categoryId == id)
+}
 
 
 
@@ -39,76 +62,3 @@ let gallery = document.createElement('div');
 gallery.classList.add('gallery');
 document.getElementById('portfolio').appendChild(gallery);
 
-
-
-
-//filtrer les travaux
-// const arrayResponse = await fetch('')
-// const filterTous = document.querySelector('#select0');
-
-// filterTous.addEventListener('click', function(){
-//     const tous = 
-// })
-
-
-// fetch('http://localhost:5678/api/categories')
-//     .then(res => res.json())
-//     .then(data => data.forEach(category => {
-//         console.log(category);
-//         const tous = document.querySelector('#select0');
-        
-//     tous.addEventListener('click', function(){
-//         const travauxFilter = category.filter(function(categories){
-//         return categories.category >= 0;
-//         console.log(travauxFilter);
-//         })
-//     })
-// }))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// for (let i = 0; i < 4; i++) {
-//     const categorie = document.createElement('div');
-//     categorie.className = 'select';
-//     categorie.id = 'select' +i;
-//     document.querySelector('.filter_travaux').appendChild(categorie);
-// }
-
-
-// let texte = document.createElement('p');
-// texte.classList.add('texte');     
-// document.querySelector('.select').appendChild(texte);      
-
-
-//     let texte = document.createElement('p');
-//     texte.classList.add('texte');
-// document.querySelectorAll('.select').appendChild(texte);
-
-
-
-
-
-// fetch('http://localhost:5678/api/works')
-//     .then((res => res.json()))
-//     .then((data => data.filter()))
