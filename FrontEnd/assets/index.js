418a338c10c1f1af819a9db0857252c969d11bab
MAAMAR BENHADJAR <biboustre@hotmail.com>
2023-09-23 14:57:50 +0200

    ksjdbfn

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -9,7 +9,6 @@ fetch('http://localhost:5678/api/works')
     }) 
 
 
-    
 function displayGallery(datas){
     document.querySelector('.gallery').innerHTML = "";
     datas.forEach(projet => { 
@@ -25,19 +24,7 @@ function displayGallery(datas){
     })
 }
 
-
 console.log("je suis 2ème");
-console.log(projects);
-
-const cat1 = projects.filter(function(project) {
-   return project;
-});
-
-function filtreCategory(id, datas){
-    return datas.filter((data) => data.categoryId == id)
-}
-
-
 
 //Création des categories
 let travauxFiltrer = document.createElement('div');
@@ -50,7 +37,17 @@ for (let i = 0; i < 4; i++) {
     categorie.className = 'select';
     categorie.id = 'select' +i;
     document.querySelector('.filter_travaux').appendChild(categorie);
+    
 }
+
+// let lesCategorys = ["Tous", "Objets", "Appartements", "Hôtel & restaurants"]; 
+
+// for (let i = 0; i < lesCategorys.length; i++) {
+//     const category = lesCategorys[i];
+//     document.querySelector('#select').innerText = category;
+//     console.log(category);
+// }
+
 
 document.querySelector('#select0').innerText = 'Tous';
 document.querySelector('#select1').innerText = 'Objets';
@@ -58,7 +55,53 @@ document.querySelector('#select2').innerText = 'Appartements';
 document.querySelector('#select3').innerText = 'Hôtels & restaurants';
 
 
+//Création de la gallery
 let gallery = document.createElement('div');
 gallery.classList.add('gallery');
 document.getElementById('portfolio').appendChild(gallery);
 
+
+
+//Filtrer les travaux
+const objet = document.querySelector('#select1');
+
+objet.addEventListener('click', function() {
+    const travauxFiltrer1 = datas.filter(function(id, data){
+        return data.categoryId == 1;
+    });
+
+    document.querySelector('.gallery').innerHTML = '';
+    displayGallery(travauxFiltrer1);
+});
+
+
+function filtreCategory(id, datas){
+    return datas.filter((data) => data.categoryId == id)
+};
+
+// objet.addEventListener('click', function() {
+//     const categ1 = datas.filter(function(data) {
+//         return data.categoryId == 1;
+//     })
+// })
+
+
+// const categ1 = .
+// const categ2 = projects.filter(function(project) {
+//     return 
+//  });
+// const categ3 = projects.filter(function(project) {
+//     return 
+//  });
+// const categ4 = projects.filter(function(project) {
+//     return 
+//  });
+
+
+//Ajout des balise <a> pour les liens de la <nav>
+for (let i = 0; i < 4; i++) {
+   const lien = document.createElement('a');
+   document.querySelector('ul').appendChild(lien);
+   const li = document.querySelector('li');
+   lien.appendChild(li);   
+}
